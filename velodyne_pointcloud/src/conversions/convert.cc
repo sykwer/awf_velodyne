323f2e49a400409cd36a58eec8c29144ee6c2d61
David Wong <david.wong@tier4.jp>
2022-07-05 16:30:04 +0900

    fix(velodyne_vls): timestamp pointclouds with no points in them

--- velodyne_pointcloud/src/conversions/convert.cc
+++ velodyne_pointcloud/src/conversions/convert.cc
@@ -241,13 +241,18 @@ void Convert::processScan(const velodyne_msgs::msg::VelodyneScan::SharedPtr scan
 
     scan_points_xyziradt.pc->header = pcl_conversions::toPCL(scanMsg->header);
 
-    // Find timestamp from first/last point (and maybe average)?
-    double first_point_timestamp = scan_points_xyziradt.pc->points.front().time_stamp;
-    // double last_point_timestamp = scan_points_xyziradt.pc->points.back().time_stamp;
-    // double average_timestamp = (first_point_timestamp + last_point_timestamp)/2;
-    scan_points_xyziradt.pc->header.stamp =
-      pcl_conversions::toPCL(rclcpp::Time(toChronoNanoSeconds(first_point_timestamp).count()));
-      //pcl_conversions::toPCL(scanMsg->packets[0].stamp - ros::Duration(0.0));
+    if (scan_points_xyziradt.pc->points.size() > 0)
+    {
+      double first_point_timestamp = scan_points_xyziradt.pc->points.front().time_stamp;
+      scan_points_xyziradt.pc->header.stamp =
+        pcl_conversions::toPCL(rclcpp::Time(toChronoNanoSeconds(first_point_timestamp).count()));
+    }
+    else
+    {
+      scan_points_xyziradt.pc->header.stamp =
+        pcl_conversions::toPCL(scanMsg->packets[0].stamp);
+    }
+    
     scan_points_xyziradt.pc->height = 1;
     scan_points_xyziradt.pc->width = scan_points_xyziradt.pc->points.size();
   }
